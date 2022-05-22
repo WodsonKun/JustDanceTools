cd2c180957dcef473a3bbd0ea53e18a32218e808
Wodson de Andrade <wodsonkun@outlook.com>
2022-05-22 12:36:37 -0300

    Remove unnecessary tools and binaries
    PictoCutter was already included on J2D, so it's useless (needs to be updated to use Eliott's last changes from the PR)
    
    AssetCooker needs to be updated
    
    Binaries are now removed, the user will need to get them using references from each README.md

--- [CKD] PictogramCooker/pictossprite_converter.bat
+++
@@ -1,7 +0,0 @@
-py bin\pictocutter_sprite.py
-
-set ddsvar=%cd%\output_dds
-for /R %%f IN (output_png\*.png) DO bin\nvcompress -bc3 "%%f" "%ddsvar%\%%~nf.dds"
-
-set tgavar=%cd%\output_tgackd
-for /R %%f IN (output_dds\*.dds) DO bin\quickbms -o "bin\scriptDDStoCKD.bms" "%%f" "%tgavar%"
